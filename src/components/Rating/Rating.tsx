d34445a2c135acbd44b4b7c8068f3b748d4579c3
vladnen97 <darkdescent1991@gmail.com>
2023-02-11 16:25:01 +0300

    wrote typization for stars

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -2,16 +2,21 @@ import React, {useState} from 'react';
 
 
 export function Rating() {
+    type starType = {
+        id: number
+        title: string
+    };
 
-    const [value, setValue] = useState<number>(0)
+    const [value, setValue] = useState<number>(0);
 
-    const stars = [
+    const stars: Array<starType> = [
         {id: 1, title: 'star'},
         {id: 2, title: 'star'},
         {id: 3, title: 'star'},
         {id: 4, title: 'star'},
         {id: 5, title: 'star'},
-    ]
+    ];
+
 
     return (
         <div className="rating">
