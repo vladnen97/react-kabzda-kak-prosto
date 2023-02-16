2109ba2973f813de0df7ec013eca01b930072b8d
vladnen97 <darkdescent1991@gmail.com>
2023-02-16 19:39:33 +0300

    change stars

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -2,7 +2,9 @@ import React, {useState} from 'react';
 
 
 type StarProps = {
+    value: 1 | 2 | 3 | 4 | 5
     selected: boolean
+    setValue: (value: number) => void
 }
 
 export function Rating() {
@@ -11,15 +13,17 @@ export function Rating() {
 
     return (
         <div>
-            <Star selected={value > 0}/>
-            <Star selected={value > 1}/>
-            <Star selected={value > 2}/>
-            <Star selected={value > 3}/>
-            <Star selected={value > 4}/>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
         </div>
     );
 }
 
 function Star(props: StarProps) {
-    return props.selected ? <b> star </b> : <span> star </span>
+    return <span onClick={() => props.setValue(props.value)}>
+        {props.selected ? <b> star </b> : ' star '}
+    </span>
 }
