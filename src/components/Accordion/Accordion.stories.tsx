37537a79a51ba9e6ab37f3f5e90ac7760766d82d
vladnen97 <darkdescent1991@gmail.com>
2023-03-03 01:58:42 +0300

    stories for uncontrolled accordion

---
+++ src/components/Accordion/Accordion.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {Accordion} from './Accordion';
+
+export default {
+    title: 'Uncontrolled Accordion',
+    component: Accordion,
+}
+
+export const ChangebleAccordion = () => <Accordion titleName={'Menu'}/>
