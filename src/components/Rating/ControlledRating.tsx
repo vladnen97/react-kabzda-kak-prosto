513dae68fc41250ad31bd5c81dfb3abebed72d53
vladnen97 <darkdescent1991@gmail.com>
2023-03-02 04:37:38 +0300

    make controllabe rating for stories

---
+++ src/components/Rating/ControlledRating.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+type StarProps = {
+    value: 1 | 2 | 3 | 4 | 5
+    selected: boolean
+    setValue: (value: number) => void
+}
+
+type PropsType = {
+    value: number
+    setValue: (value: number) => void
+}
+
+export function ControlledRating({value, setValue}:PropsType) {
+
+
+    return (
+        <div>
+            <Star selected={value > 0} setValue={setValue} value={1}/>
+            <Star selected={value > 1} setValue={setValue} value={2}/>
+            <Star selected={value > 2} setValue={setValue} value={3}/>
+            <Star selected={value > 3} setValue={setValue} value={4}/>
+            <Star selected={value > 4} setValue={setValue} value={5}/>
+        </div>
+    );
+}
+
+function Star(props: StarProps) {
+    return <span onClick={() => props.setValue(props.value)}>
+        {props.selected ? <b> star </b> : ' star '}
+    </span>
+}
