77d5dc09c00eb9513e3fbc037e3225d10ba495e7
vladnen97 <darkdescent1991@gmail.com>
2023-03-03 02:00:30 +0300

    controlled on/off switcher

---
+++ src/components/OnOff/ControlledOnOff.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+type PropsType = {
+    on: boolean
+    setOn: (on: boolean) => void
+}
+
+export function ControlledOnOff({on, setOn}: PropsType) {
+
+
+    const switcher = {
+        marginTop: '40px',
+        borderTop: '1px solid rgb(0 , 0 ,0 , 0.3)',
+        paddingTop: '40px',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '10px',
+    }
+    const onStyle = {
+        width: '40px',
+        height: '20px',
+        border: '1px solid black',
+        padding: '4px 6px',
+        backgroundColor: on ? 'green' : 'white',
+        cursor: 'pointer'
+    };
+    const offStyle = {
+        width: '40px',
+        height: '20px',
+        border: '1px solid black',
+        padding: '4px 6px',
+        backgroundColor: on ? 'white' : 'red',
+        cursor: 'pointer'
+    };
+    const indicatorStyle = {
+        width: '15px',
+        height: '15px',
+        borderRadius: '50%',
+        border: '2px solid black',
+        backgroundColor: on ? 'green' : 'red',
+    };
+
+    return (
+        <div style={switcher}>
+            <div style={onStyle} onClick={() => setOn(true)}>On</div>
+            <div style={offStyle} onClick={() => setOn(false)}>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    );
+}
+
