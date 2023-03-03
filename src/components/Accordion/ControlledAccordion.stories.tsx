91e822e21952e9789465edb24422d8bbb7069eeb
vladnen97 <darkdescent1991@gmail.com>
2023-03-03 04:16:30 +0300

    refactor

--- src/components/Accordion/ControlledAccordion.stories.tsx
+++ src/components/Accordion/ControlledAccordion.stories.tsx
@@ -1,15 +1,31 @@
 import React, {useState} from 'react';
 import {ControlledAccordion} from './ControlledAccordion';
 import {action} from '@storybook/addon-actions';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
 
 export default {
     title: 'Accordion',
     component: ControlledAccordion,
+} as ComponentMeta<typeof ControlledAccordion>;
+
+
+const Template: ComponentStory<typeof ControlledAccordion> = (args) => <ControlledAccordion {...args}/>
+
+export const Collapsed = Template.bind({});
+Collapsed.args = {
+    titleName: 'Menu',
+    collapsed: true,
+    setCollapsed: action('accordion mode change event fired'),
+};
+
+export const Uncollapsed = Template.bind({})
+Uncollapsed.args = {
+    titleName: 'List',
+    collapsed: false,
+    setCollapsed: action('accordion mode change event fired')
 }
 
-export const CollapsedAccordion = () => <ControlledAccordion titleName={'List'} collapsed={true} setCollapsed={action('only collapsed')}/>
-export const UncollapsedAccordion = () => <ControlledAccordion titleName={'List'} collapsed={false} setCollapsed={action('only uncollapsed')}/>
-export const ChangableAccordion = () => {
+export const Changable = () => {
     const [collapsed, setCollapsed] = useState<boolean>(false)
     return <ControlledAccordion titleName={'List'} collapsed={collapsed} setCollapsed={setCollapsed}/>
 }
