a67fd0c5758f2ace02cd566f4dcfa6f479c45fa8
vladnen97 <darkdescent1991@gmail.com>
2023-03-03 23:49:54 +0300

    refactoring

--- src/components/Accordion/ControlledAccordion.stories.tsx
+++ src/components/Accordion/ControlledAccordion.stories.tsx
@@ -7,7 +7,9 @@ export default {
     title: 'Components/Accordion',
     component: ControlledAccordion,
     args: {
-        setCollapsed: action('accordion mode change event fired')
+        items: [],
+        setCollapsed: action('accordion mode change event fired'),
+        onClick: action('click event fired on this element')
     }
 } as ComponentMeta<typeof ControlledAccordion>;
 
@@ -16,17 +18,42 @@ const Template: ComponentStory<typeof ControlledAccordion> = (args) => <Controll
 
 export const Collapsed = Template.bind({});
 Collapsed.args = {
-    titleName: 'Menu',
+    titleName: 'Collapsed',
     collapsed: true,
 };
 
 export const Uncollapsed = Template.bind({})
 Uncollapsed.args = {
-    titleName: 'List',
+    titleName: 'Uncollapsed',
+    collapsed: false,
+    items: [
+        {id: 1, text: 'first item'},
+        {id: 2, text: 'second item'},
+        {id: 3, text: 'third item'},
+        {id: 4, text: 'fourth item'},
+        {id: 5, text: 'fifth item'},
+    ]
+}
+
+export const Empty = Template.bind({})
+Empty.args = {
+    titleName: 'Empty',
     collapsed: false,
 }
 
 export const Changable = () => {
     const [collapsed, setCollapsed] = useState<boolean>(false)
-    return <ControlledAccordion titleName={'List'} collapsed={collapsed} setCollapsed={setCollapsed}/>
+    const shoppingList = [
+        {id: 1, text: 'sugar'},
+        {id: 2, text: 'meat'},
+        {id: 3, text: 'bread'},
+        {id: 4, text: 'water'},
+        {id: 5, text: 'butter'},
+    ]
+
+    return <ControlledAccordion titleName={'What to buy'}
+                                collapsed={collapsed}
+                                setCollapsed={setCollapsed}
+                                items={shoppingList}
+                                onClick={action('click event fired on this element')}/>
 }
