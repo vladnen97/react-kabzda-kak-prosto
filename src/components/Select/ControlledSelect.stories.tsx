211883236cf5e306e5162e579933af6deede3e2a
vladnen97 <darkdescent1991@gmail.com>
2023-03-10 03:58:38 +0300

    rename values, add example

--- src/components/Select/ControlledSelect.stories.tsx
+++ src/components/Select/ControlledSelect.stories.tsx
@@ -10,15 +10,34 @@ export default {
 
 // const Template: ComponentStory<typeof ControlledSelect> = (args) => <ControlledSelect {...args}/>
 
-export const Selector = () => {
-    const [value, setValue] = useState<any>('None')
+export const WithValue: ComponentStory<typeof ControlledSelect> = () => {
+    const [value, setValue] = useState<any>('keycss')
 
     return <ControlledSelect value={value}
-                      onChange={setValue}
-                      items={[
-                          {value: 'JavaScript', title: 'js'},
-                          {value: 'CSS', title: 'css'},
-                          {value: 'React', title: 'react'},
-                          {value: 'Redux', title: 'redux'},
-                      ]}/>
+                             onChange={setValue}
+                             items={[
+                                 {value: 'keyjs', title: 'JavaScript'},
+                                 {value: 'keycss', title: 'CSS'},
+                                 {value: 'keyreact', title: 'React'},
+                                 {value: 'keyredux', title: 'Redux'},
+                             ]}/>
 }
+
+export const WithoutValue: ComponentStory<typeof ControlledSelect> = () => {
+    const [value, setValue] = useState<any>(null)
+
+    return <ControlledSelect value={value}
+                             onChange={setValue}
+                             items={[
+                                 {value: 'keyjs', title: 'JavaScript'},
+                                 {value: 'keycss', title: 'CSS'},
+                                 {value: 'keyreact', title: 'React'},
+                                 {value: 'keyredux', title: 'Redux'},
+                             ]}/>
+}
+
+// export const Example = () => <select>
+//     <option value="1">123</option>
+//     <option value="2">456</option>
+//     <option value="3">789</option>
+// </select>
