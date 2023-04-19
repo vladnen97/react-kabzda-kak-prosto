5a0a359c0fc1874d4752e17050b43f2809a791e5
vladnen97 <darkdescent1991@gmail.com>
2023-04-19 11:16:23 +0300

    made analog clock

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -1,11 +1,10 @@
 import React, {useEffect, useState} from 'react';
+import './Clock.css'
 
 export const Clock = () => {
     const [date, setDate] = useState(new Date())
 
     useEffect(() => {
-
-
         const timerId = setInterval(() => {
             setDate(new Date())
         }, 1000)
@@ -15,9 +14,27 @@ export const Clock = () => {
         }
     }, [])
 
+    const secondsStyle = {
+        transform: `rotate(${date.getSeconds() * 6}deg)`
+    };
+    const minutesStyle = {
+        transform: `rotate(${date.getMinutes() * 6}deg)`
+    };
+    const hoursStyle = {
+        transform: `rotate(${date.getHours() * 30}deg)`
+    };
+
     return (
-        <div>
-            {date.toLocaleTimeString()}
+        <div className={"clock"}>
+            <h3>Moscow</h3>
+            <div className={"analog-clock"}>
+                <div className={"dial seconds"} style={secondsStyle} />
+                <div className={"dial minutes"} style={minutesStyle} />
+                <div className={"dial hours"} style={hoursStyle} />
+            </div>
+            <div className={"digital-clock"}>
+                {date.toLocaleTimeString()}
+            </div>
         </div>
     );
 };
